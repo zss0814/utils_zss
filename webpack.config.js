a4490d02b3bee3da34c72c0259650a03fd809f85
zss <1750592414@qq.com>
2020-01-13 19:27:16 +0800

    22222

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,12 @@
 const path = require('path');
 module.exports = {
-  mode:'development',
+  mode:'production',
+  // mode:'development',
   entry:'./src/index.js',
   output:{
     path:path.resolve(__dirname,'dist'),
-    filename:'utils_zss.js',//生成打包后的文件名
+    // filename:'utils_zss.js',//生成打包后的文件名
+    filename:'utils_zss.min.js',
     library:'utils',//指定库的名字（在使用时用）
     libraryTarget:'umd',//使得该库可以通过多种方式访问
 
